31c45f5768d5b53f0d55c7400d00288bc445711c
Semen Gricenko <semengricenko@gmail.com>
2020-05-04 16:07:09 +0500

    Fix GetCategories.php request

--- Requests/GetCategories.php
+++ Requests/GetCategories.php
@@ -7,4 +7,6 @@ $request = new web\HTTPDataBaseRequests();
 //TODO: В $response находятся категории видосов, нужно как-то их вывести на страницу
 $response = $request->sendRequest(array(
     "DataBaseRequest" => "Categories"
-));
+), $_GET);
+
+print(substr($response, strpos($response, "\r\n\r\n")));
